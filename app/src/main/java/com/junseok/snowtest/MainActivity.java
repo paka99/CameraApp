fd44f2a841a67b364e032c198296bdda20bab9e8
paka99 <rolunoa@gmail.com>
2017-07-30 21:52:46 +0900

    Implement Preivew but cannot execute

--- app/src/main/java/com/junseok/snowtest/MainActivity.java
+++ app/src/main/java/com/junseok/snowtest/MainActivity.java
@@ -1,16 +1,77 @@
 package com.junseok.snowtest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    MyCameraSurface mSurface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSurface = (MyCameraSurface) findViewById(R.id.preview);
     }
 }
 
+class MyCameraSurface extends SurfaceView implements SurfaceHolder.Callback {
+    SurfaceHolder mHolder;
+    Camera mCamera;
 
+    public MyCameraSurface(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+    }
 
+    // 표면 생성시 카메라 오픈하고 미리보기 설정
+    public void surfaceCreated(SurfaceHolder holder) {
+        mCamera = Camera.open();
+        try {
+            mCamera.setPreviewDisplay(mHolder);
+        } catch (IOException e) {
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    // 표면 파괴시 카메라도 파괴한다.
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    // 표면의 크기가 결정될 때 최적의 미리보기 크기를 구해 설정한다.
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+        Camera.Parameters params = mCamera.getParameters();
+        List<Camera.Size> arSize = params.getSupportedPreviewSizes();
+        if (arSize == null) {
+            params.setPreviewSize(width, height);
+        } else {
+            int diff = 10000;
+            Camera.Size opti = null;
+            for (Camera.Size s : arSize) {
+                if (Math.abs(s.height - height) < diff) {
+                    diff = Math.abs(s.height - height);
+                    opti = s;
+
+                }
+            }
+            params.setPreviewSize(opti.width, opti.height);
+        }
+        mCamera.setParameters(params);
+        mCamera.startPreview();
+    }
+}

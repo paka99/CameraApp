5fad39dc3d2c18e7e7dafe32cd6b1389b844cea9
paka99 <rolunoa@gmail.com>
2017-07-31 22:21:41 +0900

    Implement Preview and Virtual Picture List by swiping

--- app/src/main/java/com/junseok/snowtest/MainActivity.java
+++ app/src/main/java/com/junseok/snowtest/MainActivity.java
@@ -3,20 +3,31 @@ package com.junseok.snowtest;
 import android.content.Context;
 import android.hardware.Camera;
 import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.content.res.Configuration;
+import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.Window;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
-    MyCameraSurface mSurface;
+    //MyCameraSurface mSurface;
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +38,116 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-
         setContentView(R.layout.activity_main);
-        mSurface = (MyCameraSurface)findViewById(R.id.preview);
+
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+    }
+
+
+    public static class PreviewFrag extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private MyCameraSurface mSurface;
+
+        public PreviewFrag() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PreviewFrag newInstance() {
+            PreviewFrag fragment = new PreviewFrag();
+            //Bundle args = new Bundle();
+            //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            //fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.frag_preview, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            //mSurface = (MyCameraSurface)rootView.findViewById(R.id.preview);
+            return rootView;
+        }
+    }
+
+    public static class PictureList extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+
+        public PictureList() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PictureList newInstance() {
+            PictureList fragment = new PictureList();
+            //Bundle args = new Bundle();
+            //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            //fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.list_picture, container, false);
+            //TextView textView = (TextView) rootView.findViewById(R.id.picture_list);
+            //textView.setText("Hello");
+            //mSurface = (MyCameraSurface)rootView.findViewById(R.id.picture_list);
+
+            return rootView;
+        }
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position) {
+                case 0:
+                    return PreviewFrag.newInstance();
+                case 1:
+                    return PictureList.newInstance();
+            }
+            return PreviewFrag.newInstance();
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            switch (position) {
+//                case 0:
+//                    return "SECTION 1";
+//                case 1:
+//                    return "SECTION 2";
+//            }
+//            return null;
+//        }
     }
 }
 
@@ -87,3 +205,6 @@ class MyCameraSurface extends SurfaceView implements SurfaceHolder.Callback {
         mCamera.startPreview();
     }
 }
+
+
+

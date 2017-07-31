9e473d0f4d6d7cc13748ee1726b9cbe66dd747bd
paka99 <rolunoa@gmail.com>
2017-07-31 23:21:02 +0900

    Implement to be able to take a picture, But there is TODO item

--- app/src/main/java/com/junseok/snowtest/MainActivity.java
+++ app/src/main/java/com/junseok/snowtest/MainActivity.java
@@ -1,9 +1,12 @@
 package com.junseok.snowtest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,14 +14,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.AttributeSet;
 import android.util.Log;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 import android.view.LayoutInflater;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -43,23 +52,17 @@ public class MainActivity extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        // TODO: 오른쪽으로 스와이프 하면 사진 리스트를 띄우고 싶은데 0번 Fragment가 화면에 출력되지 않아 조사해봐야함.
+        mViewPager.setCurrentItem(1);
     }
 
 
     public static class PreviewFrag extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
         private MyCameraSurface mSurface;
 
         public PreviewFrag() {
         }
 
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
         public static PreviewFrag newInstance() {
             PreviewFrag fragment = new PreviewFrag();
             //Bundle args = new Bundle();
@@ -74,10 +77,57 @@ public class MainActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.frag_preview, container, false);
             //TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             //textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            //mSurface = (MyCameraSurface)rootView.findViewById(R.id.preview);
+            mSurface = (MyCameraSurface)rootView.findViewById(R.id.preview);
+
+            mSurface.setOnClickListener(new MyCameraSurface.OnClickListener() {
+                public void onClick(View v) {
+                    mSurface.mCamera.takePicture(null, null, mPicture);
+                }
+            });
+
             return rootView;
         }
+
+        Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+            public void onPictureTaken(byte[] data, Camera camera) {
+                Log.v("CallCamera", "Take Picure!!!");
+                String sd = Environment.getExternalStorageDirectory().getAbsolutePath() + "/SnowTest";
+                long now = System.currentTimeMillis();
+                Date date = new Date(now);
+                SimpleDateFormat sdfNow = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                String formatDate = sdfNow.format(date);
+
+                File dirPath = new File(sd);
+                if(!dirPath.exists())   dirPath.mkdirs();
+
+
+                String path = sd + "/" + formatDate + ".jpg";
+
+                File file = new File(path);
+                try {
+                    FileOutputStream fos = new FileOutputStream(file);
+                    fos.write(data);
+                    fos.flush();
+                    fos.close();
+                } catch (Exception e) {
+                    Toast.makeText(getActivity(), "파일 저장 중 에러 발생 : " +
+                            e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri uri = Uri.parse("file://" + path);
+                intent.setData(uri);
+                getActivity().sendBroadcast(intent);
+
+                Toast.makeText(getActivity(), "사진 저장 완료 : " + path,
+                        Toast.LENGTH_SHORT).show();
+                mSurface.mCamera.startPreview();
+            }
+        };
     }
+
+
 
     public static class PictureList extends Fragment {
         /**
@@ -123,13 +173,13 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            switch (position) {
-                case 0:
-                    return PreviewFrag.newInstance();
-                case 1:
-                    return PictureList.newInstance();
+            if(position == 1){
+                return PreviewFrag.newInstance();
             }
-            return PreviewFrag.newInstance();
+            else {
+                return PictureList.newInstance();
+            }
+            //Log.e("aaa", "ThisPositionis");
         }
 
         @Override
